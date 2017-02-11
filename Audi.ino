ca7df25952a86a76352f77151ac010e2022a9b0b
Heart-travel <zcgacbc@gmail.com>
2017-02-11 17:11:34 +0800

    If the event is trigger, it can send a message

--- Audi.ino
+++ Audi.ino
@@ -1,27 +1,40 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(10, 11); // RX, TX
+int CapPin = 7;
+SoftwareSerial AudiSerial(10, 11); // RX, TX
+boolean state, lastState;
 
 void setup() {
+  pinMode(CapPin, INPUT);
+  state = digitalRead(CapPin);
+  lastState = state;
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    Serial.read(); // wait for serial port to connect. Needed for native USB port only
   }
 
-
-  Serial.println("Enter AT commands:");
-
   // set the data rate for the SoftwareSerial port
-  mySerial.begin(9600);
+  AudiSerial.begin(9600);
+  Serial.println("Waiting for the event");
 }
 
 void loop() { // run over and over
-  if (mySerial.available()) {
-    Serial.write(mySerial.read());
-  }
+
+  lastState = state;
+  state = digitalRead(CapPin);
   
-  if (Serial.available()) {
-    mySerial.write(Serial.read());
+  if ( state != lastState ) {
+    Serial.println("Event is triggered");
+    AudiSerial.write("AT+CMGF=1\r\n");
+    delay(2000);
+    AudiSerial.write("AT+CMGS=\"xxxxxxxxxx\"\r\n");
+    delay(500);
+    AudiSerial.write("Switch was turned ");
+    AudiSerial.write(state ? "on" : "off");
+    AudiSerial.write(0x1A);
+    Serial.println("Finish to sent message");
   }
+
+  delay(1000);
 }
